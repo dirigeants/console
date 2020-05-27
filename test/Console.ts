b71f22ad678ea1a05e8d83cc085de25270e0eb44
Gryffon Bellish <owenbellish@gmail.com>
2020-05-27 14:59:56 -0400

    some progress done, taking a break for now

--- test/Console.ts
+++ test/Console.ts
@@ -1,5 +1,6 @@
 import ava from 'ava';
 import { KlasaConsole } from '../src';
+import { makeWritable } from './lib';
 
 /* eslint-disable dot-notation */
 ava('Default stdout is process.stdout', (test): void => {
@@ -9,11 +10,25 @@ ava('Default stdout is process.stdout', (test): void => {
 
 ava('Options set properly', (test): void => {
 	const console = new KlasaConsole({ useColor: true, timestamps: 'HH:mm:ss' });
-	test.true(typeof console['timestamp'] === 'string');
+	test.is(typeof console['timestamp'], 'string');
 	console.utc = true;
-	test.true(typeof console['timestamp'] === 'string');
+	test.is(typeof console['timestamp'], 'string');
 
 	const console2 = new KlasaConsole({ timestamps: false });
-	test.true(typeof console2['timestamp'] !== 'string');
+	test.is(console2['timestamp'], null);
 });
+
+ava('Writing to console works properly', (test): void => {
+	// Need to do 1 with the timestamp for branches.
+	const consoleWithTimestamp = new KlasaConsole({ stdout: makeWritable(1) });
+	test.notThrows((): void => consoleWithTimestamp['write'](['Hello']));
+
+	const stdout = makeWritable(6);
+	const console = new KlasaConsole({ stdout, timestamps: false });
+	for (const type of ['log', 'warn', 'error', 'debug', 'verbose', 'wtf'] as const) {
+		console[type]('Hello, world!');
+	}
+
+	global.console.log(stdout.writtenData);
+})
 /* eslint-enable dot-notation */
--- test/lib/index.ts
+++ test/lib/index.ts
@@ -1,12 +1,23 @@
-import { Writable } from 'stream';
+import { Writable as WritableStream } from 'stream';
 
-export class MockWritable extends Writable {
+class MockWritable extends WritableStream {
 
-	public writtenData: string[] = [];
+    public writtenData: string[] = [];
 
-	public _write(chunk: Buffer | string, _encoding: string, next: () => void): void {
-		this.writtenData.push(chunk.toString());
-		return next();
-	}
+    private written = 0;
 
+    public constructor(private amountToWrite: number = Infinity) { // eslint-disable-line useless-constructor
+        super();
+    }
+
+    public _write(chunk: Buffer | string, _encoding: string, next: () => void): void {
+        this.writtenData.push(chunk.toString());
+        if (++this.written >= this.amountToWrite) return this.end();
+        else return next();
+    }
+
+}
+
+export function makeWritable(amountToWrite?: number): NodeJS.WriteStream & MockWritable {
+    return new MockWritable(amountToWrite) as unknown as NodeJS.WriteStream & MockWritable;
 }
